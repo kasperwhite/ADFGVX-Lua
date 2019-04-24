5fc95da08e4571dd1bdedf6a5c6e93242c4901d5
kasperwhite <keriteabin@gmail.com>
2019-04-24 10:08:19 +0700

    #5

--- main.lua
+++ main.lua
@@ -2,102 +2,135 @@
 wwf = require("modules.wwf")
 parse = require("modules.parse")
 crypto = require("modules.crypto")
--- считывание информации с файлов 
+-- считывание данных с файлов
 local adf = wwf.read("service/adf.hed")
 local alphabet = wwf.read("service/alphabet.txt")
--- имена файлов по умолчанию
+-- установка имен по умолчанию 
 local defaultCipherFilename = "cipher.cph"
 local defaultInitFilename = "initial_message.in"
 local defaultRandomTableFilename = "random_table.tb"
--- считывание параметра "операция"
+-- считывание первой операции
 local operation = arg[1]
 
+-- проверка того, не заходит ли алфавит за границы матрицы
 if (string.len(adf) ^ 2) > string.len(alphabet) then
-    -- операция отсутствует (справка)
+    -- если операция не задана (справка)
     if operation == nil then
         print("For encrypt type:\n      enc <filepath> <password path> <mode> [table path] [outputpath]")
         print("For decrypt type:\n      dec <filepath> <password path> <mode> <table path> [outputpath]")
         print("Available modes:\n       For random fill type: rand\n       For password fill type: pass")
-    -- операция шифрования
+    -- если операция шифрования
     elseif operation == "enc" then
-        -- параметр пути к файлу с сообщением
+        -- считывание параметра с путем к файлу с сообщением
         local filepath = arg[2]
+        -- проверка задан ли параметр с путем/наличия файла/его заполненности
         if filepath ~= nil and wwf.exists(filepath) and not(wwf.empty(filepath)) then
-            -- параметр пути к файлу с паролем
+            -- считывание параметра с путем к паролю
             local passpath = arg[3]
+            -- проверка задан ли параметр с путем/наличия файла/его заполненности
             if passpath ~= nil and wwf.exists(passpath) and not(wwf.empty(passpath)) then
+                -- считывание параметра с режимом
                 local mode = arg[4]
+                -- проверка известности параметра
                 if mode ~= "rand" and mode ~= "pass" then
                     print("Unknown mode")
                 else
+                    -- если режим "случайный"
                     if mode == "rand" then
+                        -- считывание параметра с путем к файлу со случайной таблицей
                         tabpath = arg[5]
+                        -- если не указан, то установка его в значение по умолчанию
                         if tabpath == nil then
                             tabpath = defaultRandomTableFilename
                         end
                     end
+                    -- считывание параметра с путем к выходному файлу
                     local outputpath = arg[6]
+                    -- если не указан, то установка его в значение по умолчанию
                     if outputpath == nil then
                         outputpath = filepath .. ".cph"
                     end
-                    -- считывание данных
+                    -- считывание сообщения и пароля
                     local message = wwf.read(filepath)
                     local password = wwf.read(passpath)
+                    -- шифрование
                     local cipher = crypto.encrypt(adf, alphabet, mode, password, tabpath, message)
+                    -- запись шифрогаммы в файл
                     wwf.write(outputpath, cipher)
                 end
             else
+                -- вывод ошибки об инвалидности пароля
                 print("Password file path is not valid or file is empty!")
             end 
         else
+            -- вывод ошибки об инвалидности сообщения
             print("Message file path is not valid or file is empty!")
         end
-    -- операция дешифрования
+    -- если операция дешифрования
     elseif operation == "dec" then
-        -- параметр пути к файлу с шифрограммой
+        -- считывание параметра с путем к файлу с сообщением
         local filepath = arg[2]
+        -- проверка задан ли параметр с путем/наличия файла/его заполненности
         if filepath ~= nil and wwf.exists(filepath) and not(wwf.empty(filepath)) then
-            -- параметр пути к файлу с паролем
+            -- считывание параметра с путем к паролю
             local passpath = arg[3]
+            -- проверка задан ли параметр с путем/наличия файла/его заполненности
             if passpath ~= nil and wwf.exists(passpath) and not(wwf.empty(passpath)) then
+                -- считывание параметра с режимом
                 local mode = arg[4]
+                -- если режим "случайный"
                 if mode == "rand" then
+                    -- считывание параметра с путем к файлу со случайной таблицей
                     tabpath = arg[5]
+                    -- проверка задан ли параметр с путем/наличия файла/его заполненности
                     if tabpath ~= nil and wwf.exists(tabpath) and not(wwf.empty(tabpath)) then
+                        -- считывание параметра с путем к выходному файлу
                         local outputpath = arg[6]
+                        -- если не указан, то установка его в значение по умолчанию
                         if outputpath == nil then
                             outputpath = filepath .. ".in"
                         end
-                        -- считывание данных
+                        -- считывание шифрограммы и пароля из файлов
                         local cipher = wwf.read(filepath)
                         local password = wwf.read(passpath)
+                        -- дешифрование
                         local initMes = crypto.decrypt(adf, alphabet, mode, password, tabpath, cipher)
+                        -- запись исходного сообщения в файл
                         wwf.write(outputpath, initMes)
                     else
+                        -- сообщение об инвалидности пути к файлу со случайной таблицей
                         print("Table file path is not valid or file is empty!")
                     end
-
+                -- проверка известности параметра
                 elseif mode ~= "rand" and mode ~= "pass" then
                     print("Unknown mode")
+                -- если режим "случайный"
                 elseif mode == "pass" then
+                    -- считывание параметра с путем к выходному файлу
                     local outputpath = arg[5]
+                    -- если не указан, то установка его в значение по умолчанию
                     if outputpath == nil then
                         outputpath = filepath .. ".in"
                     end
-                    -- считывание данных
+                    -- считывание шифрограммы и пароля из файлов
                     local cipher = wwf.read(filepath)
                     local password = wwf.read(passpath)
+                    -- дешифрование
                     local initMes = crypto.decrypt(adf, alphabet, mode, password, "", cipher)
+                    -- запись исходного сообщения в файл
                     wwf.write(outputpath, initMes)
                 end
             else
+                -- вывод ошибки об инвалидности пароля
                 print("Password file path is not valid or file is empty!")
             end 
         else
+            -- вывод ошибки об инвалидности сообщения
             print("Message file path is not valid or file is empty!")
         end
     end
 else
+    -- вывод ошибки о выходе алфавита за границы матрицы
     print("Your alphabet goes beyond the bounds of the matrix")
 end
 
--- modules/crypto.lua
+++ modules/crypto.lua
@@ -2,14 +2,15 @@ parse = require("modules.parse")
 wwf = require("modules.wwf")
 
 local crypto = {}
--- функция, возвращающая таблицу замен
+-- функция, возвращающая таблицу перестановок учитывая режим
 function crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
+    -- генерация матрицы замен, если выбран режим "по паролю"
     if mode == "pass" then
         stlb = adf
         tab = {}
         count = 1
         alphArr = parse.stringToTab(alphabet)
-        --удаление повторяющихся символов в алфавите
+        -- конкатенация пароля с алфавитом, из которого исключены буквы первого
         newAlphArr = {}
         for i = 1, #alphArr do
             if string.find(pass, alphArr[i]) == nil then
@@ -17,7 +18,7 @@ function crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
             end
         end
         newAlph = pass .. parse.tabToString(newAlphArr)
-        --составляем матрицу замен основанную на пароле
+        -- заполнение матрицы "новым алфавитом"
         for i = 1, string.len(stlb) do
             table.insert( tab, #tab + 1, {})
             for j = 1, string.len(stlb) do
@@ -29,14 +30,16 @@ function crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
                 count = count + 1 
             end
         end
-
         return tab
+    -- генерация матрицы замен, если выбран режим "случайно"
     elseif mode == "rand" then
         met = 1
+        -- рандомизация значений на основе системного времени
         math.randomseed(os.time())
         math.random() math.random() math.random()
         alphArr = parse.stringToTab(alphabet)
         ranTab = {}
+        -- заполнение матрицы случайными буквами 
         for i = 1, string.len(adf) do
             table.insert(ranTab, #ranTab + 1, {})
             for j = 1, string.len(adf) do
@@ -49,15 +52,15 @@ function crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
                 end
                 met = met + 1
             end
+            -- запись таблицы в файл для дешифрования 
             wwf.write(tabpath, parse.tabToString(ranTab[i]))
         end
-
         return ranTab
     else 
         print("Unknown mode")
     end
 end
--- функция, возвращающая замененные буквы
+-- функция возвращающая замененные буквы
 function crypto.replaceChar(adf, tab, str)
     mes = ""
     for s = 1, string.len(str) do
@@ -71,10 +74,12 @@ function crypto.replaceChar(adf, tab, str)
     end
     return mes
 end
--- функция, генерирующая таблицу перестановки и возвращающая конечную шифрограмму
+-- функция генерирующая переставленную матрицу. 
+-- перестановка осуществляется в зависимости от ключевого слова
 function crypto.getPermTable(adf, pass, str)
     perm = {}
     count = 1
+    -- генерация матрицы с замененными буквами
     for i = 1, math.ceil((string.len(str) / string.len(pass))) do
         table.insert(perm, #perm + 1, {})
         for j = 1, string.len(pass) do
@@ -82,7 +87,7 @@ function crypto.getPermTable(adf, pass, str)
             count = count + 1
         end
     end
-
+    -- сопоставление буквы пароля со столбцом
     passCorColumn = {}
     for j = 1, string.len(pass) do
         col = {}
@@ -91,18 +96,17 @@ function crypto.getPermTable(adf, pass, str)
         end
         passCorColumn[string.sub(pass, j, j)] = parse.tabToString(col)
     end
-
+    -- сортировка пароля по алфавиту
     passSort = parse.stringToTab(pass)
     table.sort(passSort)
-
+    -- заполнение зашифрованного сообщения
     message = ""
     for i = 1, #passSort do
         message = message .. passCorColumn[passSort[i]]
     end
-
     return message
 end
--- главная функция шифрования 
+-- главная функция шифрования. собирает все шаги шифрования
 function crypto.encrypt(adf, alphabet, mode, pass, tabpath, mess)
     repTab = crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
     replaceChars = crypto.replaceChar(adf, repTab, mess)
@@ -112,10 +116,12 @@ function crypto.encrypt(adf, alphabet, mode, pass, tabpath, mess)
 end
 
 function crypto.decrypt(adf, alphabet, mode, pass, tabpath, ciph)
+    -- нахождения числа самых длинных столбцов
     mostLong = string.len(ciph) % string.len(pass)
+    -- нахождение длины "нормальных" столбцов
     normLen = math.floor(string.len(ciph) / string.len(pass))
 
-    -- установка соответствий буква-количество эл-ов в его столбце
+    -- сопоставление длины столбца букве пароля
     passTab = {}
     for i = 1, mostLong do
         passTab[string.sub(pass, i, i)] = normLen + 1
@@ -124,16 +130,16 @@ function crypto.decrypt(adf, alphabet, mode, pass, tabpath, ciph)
         passTab[string.sub(pass, i, i)] = normLen
     end
 
-    -- создание сортированного пароля
+    -- сортировка пароля по алфавиту
     passArrSort = parse.stringToTab(pass)
     table.sort(passArrSort)
 
-    -- установка соответствий буква-длина стобца для сортированной таблицы 
+    -- сопоставление длины столбца букве пароля для сортированного пароля
     passArrSortCor = {}
     for i = 1, #passArrSort do
         passArrSortCor[passArrSort[i]] = passTab[passArrSort[i]]
     end
-    -- считывание строк из шифрограммы определенными частями
+    -- считывание шифрограммы частями определенной длины
     passSortCorColumn = {}
     currentId = 1
     for i = 1, #passArrSort do
@@ -141,7 +147,7 @@ function crypto.decrypt(adf, alphabet, mode, pass, tabpath, ciph)
         currentId = currentId + passArrSortCor[passArrSort[i]]
     end
 
-    -- установка соответствий буква-столбец для стандартной таблицы 
+    -- сопоставление столбцов буквам пароля
     passCorColumn = {}
     columnArr = {}
     passArr = parse.stringToTab(pass)
@@ -150,14 +156,14 @@ function crypto.decrypt(adf, alphabet, mode, pass, tabpath, ciph)
         table.insert(columnArr, #columnArr + 1, parse.stringToTab(passSortCorColumn[passArr[i]]))
     end
 
-    -- сбор криптограммы в один массив
+    -- создание массива с частями шифрограммы, расположенными последовательно
     cryptArr = {}
     for j = 1, #columnArr[1]  do
         for i = 1, #columnArr do
             table.insert(cryptArr, #cryptArr + 1, columnArr[i][j])
         end
     end
-    -- расшифровка криптограммы в соответствии с таблицей замен
+    -- получение матрицы замен в зависимости от режима
     local repTab = {}
     if mode == "pass" then
         repTab = crypto.getReplaceTable(adf, alphabet, mode, pass, tabpath)
@@ -166,7 +172,7 @@ function crypto.decrypt(adf, alphabet, mode, pass, tabpath, ciph)
     else
         print("Unknown mode")
     end
-
+    -- заполнение расшифрованного сообщения
     cryptMes = ""
     for i = 1, #cryptArr, 2 do
         cryptMes = cryptMes .. repTab[string.find(adf, cryptArr[i])][string.find(adf, cryptArr[i+1])]
